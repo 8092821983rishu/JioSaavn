28fbc2ac83b18cc26cb1bca745ad874d9939e129
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-07-16 17:39:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,6 +24,10 @@ LOGGER_ID = int(getenv("LOGGER_ID", ""))
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "5050578106"))
 
+#Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
+YT_API_KEY = getenv("YT_API_KEY" , 'xbit_0000403730835845267241' ) ## get your secret key from Mr cute x - @MR_CUTE_X
+
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
